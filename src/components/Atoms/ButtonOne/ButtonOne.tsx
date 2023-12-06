793b3adbf275f95b14a1aa08ab87554bc39c9269
Evgen <zhenyalezhenin@gmail.com>
2023-12-06 22:25:45 +0300

    footerWithoutLogic

---
+++ src/components/Atoms/ButtonOne/ButtonOne.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './ButtonOne.module.scss';
+
+export type props = {
+  title?: string;
+  className?: string;
+  btnStyle?: string;
+};
+
+const ButtonOne: React.FC<props> = (props) => {
+  return (
+    <div className={[styles.btn_container, props.className].join(' ')}>
+      <button className={[styles.btn, props.btnStyle].join(' ')}>
+        {props.title}
+      </button>
+    </div>
+  );
+};
+
+export default ButtonOne;
--- src/components/Templates/home/Home.tsx
+++ src/components/Templates/home/Home.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Header from '../../Organisms/Header/Header.tsx';
 import Footer from '../../Organisms/Footer/Footer.tsx';
+import ProductPreview from '../../Organisms/ProductPreview/ProductPreview.tsx';
 
 const Home: React.FC = () => (
   <div>
     <Header />
+    <ProductPreview />
     <Footer />
   </div>
 );
