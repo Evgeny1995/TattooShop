07cf98a94847149d6b03fcaf29082678017cb429
Evgen <zhenyalezhenin@gmail.com>
2023-12-07 17:03:53 +0300

    HomeCalog

--- src/components/Templates/home/Home.tsx
+++ src/components/Templates/home/Home.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import Header from '../../Organisms/Header/Header.tsx';
 import Footer from '../../Organisms/Footer/Footer.tsx';
 import ProductPreview from '../../Organisms/ProductPreview/ProductPreview.tsx';
+import HomeCatalog from '../../Molecules/HomeCatalog/HomeCatalog.tsx';
 
 const Home: React.FC = () => (
   <div>
     <Header />
     <ProductPreview />
+    <HomeCatalog />
     <Footer />
   </div>
 );
