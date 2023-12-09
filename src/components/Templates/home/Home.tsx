aaac8a686baa771c2d16c6dfb12f2f3aed7943cf
Evgen <zhenyalezhenin@gmail.com>
2023-12-09 14:03:03 +0300

    HomeSeasonalOffers

--- src/components/Templates/home/Home.tsx
+++ src/components/Templates/home/Home.tsx
@@ -4,12 +4,14 @@ import Header from '../../Organisms/Header/Header.tsx';
 import Footer from '../../Organisms/Footer/Footer.tsx';
 import ProductPreview from '../../Organisms/ProductPreview/ProductPreview.tsx';
 import HomeCatalog from '../../Molecules/HomeCatalog/HomeCatalog.tsx';
+import HomeSeasonalOffers from '../../Molecules/HomeSeasonalOffers/HomeSeasonalOffers.tsx';
 
 const Home: React.FC = () => (
   <div>
     <Header />
     <ProductPreview />
     <HomeCatalog />
+    <HomeSeasonalOffers />
     <Footer />
   </div>
 );
